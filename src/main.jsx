e554d556c56c60cbbe3fe60ee50eec9c56416044
fgb <fgb@fgbDebian.pgonbo.com>
2024-01-24 23:50:43 -0600

    fuente
    se instala paquete de fuente

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// Supports weights 200-800
+import '@fontsource-variable/dosis';
 import App from './App.jsx'
 import './index.css'
 
