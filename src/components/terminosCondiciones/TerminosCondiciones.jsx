deb6dff46fc243e912983f161960c63fa1362b33
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2024-02-15 22:27:28 -0600

    terminos
    agregando contenido a terminos y condiciones

--- src/components/terminosCondiciones/TerminosCondiciones.jsx
+++ src/components/terminosCondiciones/TerminosCondiciones.jsx
@@ -1,11 +1,117 @@
 import Template from "../template/Template";
+import "./TerminosCondiciones.css";
 
-const TerminosCondiciones=()=>{
-  return (
-    <Template>
-        Términos y condiciones
-    </Template>
-  )
-}
+const TerminosCondiciones = () => {
+	return (
+		<Template>
+			<h1>Condiciones de servicio</h1>
+			<div className="terminos-body">
+				<section>
+					<h2>1. Introducción</h2>
+					<p>
+						Las presentes condiciones de servicio regulan el acceso y la
+						utilización de la página web [MPT], titularidad de [Francisco
+						Gonzales, Itzel Cruz y Octavio Reyes]{" "}
+					</p>
+				</section>
+				<section>
+					<h2>2. Objeto</h2>
+					<p>
+						La Página web tiene por objeto facilitar a los usuarios información
+						sobre aquellos temas relacionados al PvP de Pokémon Go, dando pie a
+						aquellos jugadores que quieran incursionar en este gran mundo.
+					</p>
+				</section>
+				<section>
+					<h2>3. Acceso</h2>
+					<p>
+						El acceso a la Página web es gratuito, salvo en aquellos casos en
+						los que se requiera la contratación de un servicio específico.
+					</p>
+				</section>
+				<section>
+					<h2>4. Utilización</h2>
+					<p>
+						El usuario se compromete a utilizar la Página web de conformidad con
+						la ley, la moral, las buenas costumbres y el orden público. El
+						usuario se abstendrá de utilizar la Página web para la realización
+						de actividades ilícitas o contrarias a la buena fe y al orden
+						público, que atenten contra los derechos de terceros o que puedan
+						dañar, inutilizar, sobrecargar o deteriorar la Página web o sus
+						servicios.
+					</p>
+				</section>
+				<section>
+					<h2>5. Propiedad intelectual e industrial</h2>
+					<p>
+						Los contenidos de MPT están protegidos por la legislación en materia
+						de propiedad intelectual e industrial. El usuario se abstendrá de
+						utilizar los contenidos de la MPT para la realización de actividades
+						ilícitas o contrarias a la buena fe y al orden público, que atenten
+						contra los derechos de terceros o que puedan dañar, inutilizar,
+						sobrecargar o deteriorar los contenidos de la Página web o sus
+						servicios.
+					</p>
+					<p>
+						El usuario se compromete a no reproducir, copiar, distribuir,
+						comunicar públicamente, transformar o modificar los contenidos de la
+						MPT, salvo en los casos autorizados por la ley o por el titular de
+						los derechos.
+					</p>
+				</section>
+				<section>
+					<h2>6. Responsabilidades</h2>
+					<p>
+						Los titulares de MPT no se responsabilizan de los daños y perjuicios
+						de cualquier naturaleza que pudieran ocasionarse por el uso de la
+						página web, y en particular, por los daños derivados de la falta de
+						disponibilidad, mantenimiento y continuidad del funcionamiento o de
+						sus servicios, así como por la transmisión de virus o programas
+						maliciosos o lesivos en los contenidos de la Página web o en los
+						servicios prestados a través de la misma. Los titulares de MPT
+						tampoco se responsabilizan de los daños y perjuicios derivados del
+						uso de los servicios de la página web por parte de los usuarios o de
+						la falta de veracidad, exactitud, exhaustividad y actualidad de los
+						contenidos. El titular de la Página web se reserva el derecho a
+						suspender, modificar o interrumpir el acceso a la Página web o a sus
+						servicios, en cualquier momento y sin previo aviso, por motivos
+						técnicos o de cualquier otra índole.
+					</p>
+				</section>
+				<section>
+					<h2>7. Enlaces a otras páginas web</h2>
+					<p>
+						MPT puede incluir enlaces a otras páginas web. Los titulares no se
+						responsabilizan de los contenidos de estas páginas web, ni de los
+						daños y perjuicios que pudieran ocasionarse por el uso de las
+						mismas.
+					</p>
+				</section>
+				<section>
+					<h2>8. Legislación aplicable</h2>
+					<p>
+						Las presentes condiciones de servicio se rigen por la legislación
+						mexicana.
+					</p>
+				</section>
+				<section>
+					<h2>9. Modificación de las condiciones de servicio</h2>
+					<p>
+						Los titulares de MPT se reservan el derecho a modificar en cualquier
+						momento las presentes condiciones de servicio.
+					</p>
+				</section>
+				<section>
+					<h2>10. Jurisdicción competente</h2>
+					<p>
+						Las partes, con renuncia expresa a cualquier otro fuero, se someten
+						a la jurisdicción de los Juzgados y Tribunales de la Ciudad de
+						México.
+					</p>
+				</section>
+			</div>
+		</Template>
+	);
+};
 
 export default TerminosCondiciones;
