e2ced976ccd8bd9fcdfef20f6bd108397d3c786a
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2024-02-16 22:05:21 -0600

    jugando
    cambio de nombre

--- src/data/rankingMundial.js
+++ src/data/rankingMundial.js
@@ -1,6 +1,6 @@
 export const RANKING_MUNDIAL=[
     {nombre:'Pgonbo', equipo:'instinto', puntos:3425},
-    {nombre:'jugador2', equipo:'valor', puntos:3350},
+    {nombre:'prueba', equipo:'azul', puntos:30},
     {nombre:'jugador3', equipo:'valor', puntos:3200},
     {nombre:'jugador4', equipo:'valor', puntos:3150},
     {nombre:'jugador5', equipo:'valor', puntos:3100}, 
