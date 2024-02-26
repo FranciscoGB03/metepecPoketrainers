1ac4d849c28c04300c3b2a572f0851afc9eea3c4
moonclau <ccruzh@toluca.tecnm.mx>
2024-02-25 22:21:51 -0600

    toolpic

--- src/components/rankingRegional/RankingRegional.jsx
+++ src/components/rankingRegional/RankingRegional.jsx
@@ -106,9 +106,17 @@ function RankingRegional() {
                         </TableCell>
                         <TableCell className="border-b border-gray-200 bg-white text-sm">{reg.equipo_pokemon.map(pokemon=>
                           <Fragment key={pokemon.id}>
-                          <strong>{pokemon.nombre}</strong><br/>
-                          <span>ataque basico: {pokemon.ataque_basico}</span><br/>
-                          <span>ataques cargados: {pokemon.ataque_cargado1}, {pokemon.ataque_cargado2}</span><br/>
+                          <div className="flex items-center justify-center ">
+                            <div className="relative inline-block tooltip  my-2 ">
+                             <a to="" className="hover:text-gray-400 font-medium"><strong>{pokemon.nombre}</strong></a>
+                              <div className="flex flex-col bg-orange-500 w-60 h-auto rounded-md z-20 absolute right-0 invisible tooltip-item pl-4">
+                                <span className="mt-4">ataque basico: {pokemon.ataque_basico}</span><br/>
+                                <span className="mb-4">ataques cargados: {pokemon.ataque_cargado1}, {pokemon.ataque_cargado2}</span>
+                                
+                              </div>
+                            </div>
+                          </div>
+
                           </Fragment>
                           )}    
                         </TableCell>
@@ -136,45 +144,6 @@ function RankingRegional() {
         </div>
       </div>
 
-      {/* <div className="ranreg-body">
-        <div>
-          <h3>Ranking Regional</h3>
-        </div>
-        
-        <div className="m-8 sm:-mx-8 px-4 sm:px-8 py-4">
-          <div className="inline-block min-w-full">
-            <Table className="leading-normal " >
-              <TableHead>
-                <TableRow>
-                  <TableHeaderCell className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Jugador</TableHeaderCell>
-                  <TableHeaderCell className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Equipo
-                  </TableHeaderCell>
-                  <TableHeaderCell className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">Puntos</TableHeaderCell>
-                  <TableHeaderCell className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider mr-4">Equipo Pokemon</TableHeaderCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {RANKING_REGIONAL.map(reg=>(
-                <TableRow key={reg.id}>
-                <TableCell className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{reg.nombre}</TableCell>
-                <TableCell className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">{reg.equipo}</TableCell>
-                <TableCell className="px-5 py-5 border-b border-gray-200 bg-white text-sm">{reg.puntos}</TableCell>
-                <TableCell className="border-b border-gray-200 bg-white text-sm">{reg.equipo_pokemon.map(pokemon=>
-                <Fragment key={pokemon.id}>
-                <strong>{pokemon.nombre}</strong><br/>
-                <span>ataque basico: {pokemon.ataque_basico}</span><br/>
-                <span>ataques cargados: {pokemon.ataque_cargado1}, {pokemon.ataque_cargado2}</span><br/>
-                </Fragment>
-                )}</TableCell>
-              </TableRow>
-              ))}
-              </TableBody>
-            </Table>
-          </div>
-        </div>
-      </div> */}
     </Template>
   );
 }
