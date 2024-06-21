69a1da3a0a4ecf8eccad0a2f18f64ef3971f7ac4
moonclau <ccruzh@toluca.tecnm.mx>
2024-06-20 22:04:00 -0600

    modal pokemon

--- front/src/components/admin/LigaLocal/AgregarPokemonModal.jsx
+++ front/src/components/admin/LigaLocal/AgregarPokemonModal.jsx
@@ -1,12 +1,23 @@
 import PropTypes from "prop-types";
 import { PokemonCompetidor } from "../models/models";
 import { useEffect, useState } from "react";
+import { SlArrowDown } from "react-icons/sl";
+
 /**
  * Modal para agregar pokemons
- * @param {isOpen} param0  
- * @returns modal para agregar un pokemon 
+ * @param {isOpen} param0
+ * @returns modal para agregar un pokemon
  */
-const AgregarPokemonModal = ({ isOpen, onClose, competidorId, pokemons, ligas, rapidos, cargados, sendRequest }) => {
+const AgregarPokemonModal = ({
+  isOpen,
+  onClose,
+  competidorId,
+  pokemons,
+  ligas,
+  rapidos,
+  cargados,
+  sendRequest,
+}) => {
   /**hooks */
   const [pokemon, setPokemon] = useState(PokemonCompetidor);
   // Asegurarse de que las props sean arrays antes de intentar mapear
@@ -35,7 +46,7 @@ const AgregarPokemonModal = ({ isOpen, onClose, competidorId, pokemons, ligas, r
   const savePokemon = async () => {
     const pokemones = new Array();
     pokemones.push(pokemon);
-    await sendRequest("POST", "/registrarEquipo", pokemones,"addPokemon");
+    await sendRequest("POST", "/registrarEquipo", pokemones, "addPokemon");
     await sendRequest("GET", "/getLigaLocal", {}, "competidores");
     onClose();
   };
@@ -44,89 +55,147 @@ const AgregarPokemonModal = ({ isOpen, onClose, competidorId, pokemons, ligas, r
   /**render */
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md">
-        <button
-          className="mb-4 text-gray-600 hover:text-gray-900"
-          onClick={()=>{onClose();}}
-        >
-          Cerrar
-        </button>
-        <div>
-          <label>
-            Pokemon:{" "}
-            <select
-              name="pokemon"
-              onChange={(e) => handleChange(e, pokemonsArray)}
-              value={pokemon.pokemon.id}
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+        <div className="relative flex w-full">
+          <h1 className="text-xl	text-gray-400 	">Agrega Pokemons</h1>
+          <button
+            className="!absolute right-1 top-1 px-3 text-center align-middle text-xl text-gray-400 hover:text-gray-900"
+            onClick={() => {
+              onClose();
+            }}
+          >
+            X
+          </button>
+        </div>
+        <hr className="h-px my-3 bg-gray-200 border-0 " />
+        <div className="w-full max-w-lg flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 p-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs mb-2"
+              htmlFor="grid-first-name"
             >
-              <option value="">Selecciona un Pokémon</option>
-              {pokemonsArray.map((poke) => (
-                <option key={poke.id} value={poke.id}>
-                  {poke.nombre}
-                </option>
-              ))}
-            </select>
-          </label>
-          <label>
-            Ataque rápido:{" "}
-            <select
-              name="ataque_rapido"
+              Pokemon:{" "}
+            </label>
+            <div className="relative flex w-full">
+              <select
+                name="pokemon"
+                className="appearance-none row-start-1 col-start-1 block w-full mt-2  border border-gray-200
+                   text-sm rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                onChange={(e) => handleChange(e, pokemonsArray)}
+                value={pokemon.pokemon.id}
+              >
+                <option value="">Selecciona un Pokémon</option>
+                {pokemonsArray.map((poke) => (
+                  <option key={poke.id} value={poke.id}>
+                    {poke.nombre}
+                  </option>
+                ))}
+              </select>
+              <SlArrowDown className="!absolute right-4 top-6 text-gray-400" />
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-3 p-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs mb-2"
+              htmlFor="grid-first-name"
+            >
+              Ataque rápido:{" "}
+            </label>
+            <div className="relative flex w-full">
+              <select
+                className="appearance-none text-sm row-start-1 col-start-1 block w-full mt-2  border border-gray-200
+                rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+               name="ataque_rapido"
               onChange={(e) => handleChange(e, rapidosArray)}
               value={pokemon.ataque_rapido.id}
-            >
+              >
               <option value="">Selecciona un Ataque Rápido</option>
               {rapidosArray.map((rapido) => (
                 <option key={rapido.id} value={rapido.id}>
                   {rapido.nombre_la}
                 </option>
               ))}
-            </select>
-          </label>
-          <label>
-            Primer ataque cargado:{" "}
-            <select
-              name="primer_ataque_cargado"
-              onChange={(e) => handleChange(e, cargadosArray)}
-              value={pokemon.primer_ataque_cargado.id}
+              </select>
+              <SlArrowDown className="!absolute right-4 top-6 text-gray-400" />
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-3 p-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs mb-2"
+              htmlFor="grid-first-name"
             >
-              <option value="">Selecciona un Primer Ataque Cargado</option>
+              Primer ataque cargado:{" "}
+            </label>
+            <div className="relative flex w-full">
+              <select
+                className="appearance-none text-sm row-start-1 col-start-1 block w-full mt-2  border border-gray-200
+                rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                name="primer_ataque_cargado"
+                onChange={(e) => handleChange(e, cargadosArray)}
+                value={pokemon.primer_ataque_cargado.id}
+              >
+              <option value="">Primer Ataque Cargado</option>
               {cargadosArray.map((cargado) => (
                 <option key={cargado.id} value={cargado.id}>
                   {cargado.nombre_la}
                 </option>
               ))}
-            </select>
-          </label>
-          <label>
-            Segundo ataque cargado:{" "}
-            <select
-              name="segundo_ataque_cargado"
+              </select>
+              <SlArrowDown className="!absolute right-4 top-6 text-gray-400" />
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-3 p-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs mb-2"
+              htmlFor="grid-first-name"
+            >
+              Segundo ataque cargado:{" "}
+            </label>
+            <div className="relative flex w-full">
+              <select
+                className="appearance-none text-sm row-start-1 col-start-1 block w-full mt-2  border border-gray-200
+                rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                name="segundo_ataque_cargado"
               onChange={(e) => handleChange(e, cargadosArray)}
               value={pokemon.segundo_ataque_cargado.id}
             >
-              <option value="">Selecciona un Segundo Ataque Cargado</option>
+              <option value="">Segundo Ataque Cargado</option>
               {cargadosArray.map((cargado) => (
                 <option key={cargado.id} value={cargado.id}>
                   {cargado.nombre_la}
                 </option>
               ))}
             </select>
-          </label>
-          <label>
-            Seleccione la liga:{" "}
-            <select
+              <SlArrowDown className="!absolute right-4 top-6 text-gray-400" />
+            </div>
+          </div>
+          <div className="w-full md:w-1/2 px-3 p-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs mb-2"
+              htmlFor="grid-first-name"
+            >
+              Seleccione la liga:{" "}
+            </label>
+            <div className="relative flex w-full">
+              <select
+                className="appearance-none text-sm row-start-1 col-start-1 block w-full mt-2  border border-gray-200
+                rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               name="liga"
               onChange={(e) => handleChange(e, ligasArray)}
               value={pokemon.liga.id}
             >
-              <option value="">Selecciona liga</option>
-              {ligasArray.map((liga) => (
-                <option key={liga.id} value={liga.id}>
-                  {liga.nombre}
-                </option>
-              ))}
-            </select>
-          </label>
+            <option value="">Selecciona liga</option>
+            {ligasArray.map((liga) => (
+              <option key={liga.id} value={liga.id}>
+                {liga.nombre}
+              </option>
+            ))}
+          </select>
+              <SlArrowDown className="!absolute right-4 top-6 text-gray-400" />
+            </div>
+          </div>
+        </div>
+        <div>
 
           <button onClick={savePokemon}>Guardar</button>
         </div>
@@ -145,7 +214,7 @@ AgregarPokemonModal.propTypes = {
   ligas: PropTypes.array,
   rapidos: PropTypes.array,
   cargados: PropTypes.array,
-  sendRequest: PropTypes.func.isRequired
+  sendRequest: PropTypes.func.isRequired,
 };
 
 export default AgregarPokemonModal;
--- front/src/components/admin/LigaLocal/LigaLocalAdmin.jsx
+++ front/src/components/admin/LigaLocal/LigaLocalAdmin.jsx
@@ -9,117 +9,134 @@ import Listado from "./Listado";
 import { getUID } from "../../auth/helpers";
 
 const LigaLocalAdmin = () => {
-	/**hooks */
-	const { data, setData, error, sendRequest } = useAxiosBack();
-	const [newCompetidor, setNewCompetidor] = useState(CompetidorModel);
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const [regId, setRegId] = useState(null);
-	//useEffect
-	useEffect(() => {
-		const fetchData = async () => {
-			await sendRequest("GET", "/getCatalogosLigaEquipos", {}, "liga");
-			await sendRequest("GET", "/getPokemons", {}, "pokes");
-			await sendRequest("GET", "/getLigaLocal", {}, "competidores");
-			await sendRequest("GET", "/getAtaquesRapidos", {}, "rapidos");
-			await sendRequest("GET", "/getAtaquesCargados", {}, "cargados");
-		};
-		fetchData();
-	}, []);
-	useEffect(() => {
-		if (newCompetidor.user_id === 0) {
-			setNewCompetidor({ ...newCompetidor, user_id: getUID() });
-		}
-	}, [newCompetidor]);
-	useEffect(() => {
-		const message =
-			error?.addPokemon?.message ||
-			error?.addCompetidor?.message ||
-			error?.competidor?.message ||
-			error?.actualizaCompetidor?.message ||
-			error?.eliminaPoke?.message;
-		if (message) {
-			setTimeout(() => {
-				showErrorAlert(`Error:${message}`);
-			}, 200);
-		}
-	}, [error]);
-	/** funciones */
-	const openModal = (id) => {
-		setRegId(id);
-		setIsModalOpen(true);
-	};
+  /**hooks */
+  const { data, setData, error, sendRequest } = useAxiosBack();
+  const [newCompetidor, setNewCompetidor] = useState(CompetidorModel);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [regId, setRegId] = useState(null);
+  //useEffect
+  useEffect(() => {
+    const fetchData = async () => {
+      await sendRequest("GET", "/getCatalogosLigaEquipos", {}, "liga");
+      await sendRequest("GET", "/getPokemons", {}, "pokes");
+      await sendRequest("GET", "/getLigaLocal", {}, "competidores");
+      await sendRequest("GET", "/getAtaquesRapidos", {}, "rapidos");
+      await sendRequest("GET", "/getAtaquesCargados", {}, "cargados");
+    };
+    fetchData();
+  }, []);
+  useEffect(() => {
+    if (newCompetidor.user_id === 0) {
+      setNewCompetidor({ ...newCompetidor, user_id: getUID() });
+    }
+  }, [newCompetidor]);
+  useEffect(() => {
+    const message =
+      error?.addPokemon?.message ||
+      error?.addCompetidor?.message ||
+      error?.competidor?.message ||
+      error?.actualizaCompetidor?.message ||
+      error?.eliminaPoke?.message;
+    if (message) {
+      setTimeout(() => {
+        showErrorAlert(`Error:${message}`);
+      }, 200);
+    }
+  }, [error]);
+  /** funciones */
+  const openModal = (id) => {
+    setRegId(id);
+    setIsModalOpen(true);
+  };
 
-	const closeModal = () => setIsModalOpen(false);
-	/**render */
-	return (
-		<div>
-			LigaLocalAdmin
-			<div className="d-flex center">
-				<h1>Competidores de liga local</h1>
-			</div>
-			<div>
-				<label>
-					Nombre:{" "}
-					<input
-						type="text"
-						name="nombre"
-						value={newCompetidor.nombre}
-						onChange={(e) => handleInputChange(e, setNewCompetidor)}
-					/>
-				</label>
+  const closeModal = () => setIsModalOpen(false);
+  /**render */
+  return (
+    <div className="p-8 items-center">
+      <h1 className="text-3xl font-semibold pb-5 text-center">
+        LigaLocalAdmin
+      </h1>
 
-				<label>
-					Equipo Insignia:{" "}
-					<select
-						name="equipo_insignia"
-						value={newCompetidor.equipo_insignia.id}
-						onChange={(e) =>
-							handleInputChange(e, setNewCompetidor, data.liga.EquipoInsignia)
-						}>
-						{data?.liga?.EquipoInsignia?.map((equipo) => (
-							<option key={equipo.id} value={equipo.id}>
-								{equipo.nombre}
-							</option>
-						))}
-					</select>
-				</label>
-				<label>
-					Puntos Iniciales:{" "}
-					<input
-						type="number"
-						name="puntos"
-						min={0}
-						value={newCompetidor.puntos}
-						onChange={(e) => handleInputChange(e, setNewCompetidor)}
-					/>
-				</label>
-				<button
-					onClick={() =>
-						agregarJugador(sendRequest, newCompetidor, setNewCompetidor)
-					}>
-					Guardar competidor
-				</button>
-			</div>
-			<div>
-				<Listado
-					data={data}
-					setData={setData}
-					openModal={openModal}
-					sendRequest={sendRequest}
-				/>
-			</div>
-			<AgregarPokemonModal
-				isOpen={isModalOpen}
-				onClose={closeModal}
-				competidorId={regId}
-				pokemons={data.pokes}
-				ligas={data?.liga?.Liga}
-				rapidos={data.rapidos}
-				cargados={data.cargados}
-				sendRequest={sendRequest}
-			/>
-		</div>
-	);
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="text-2xl font-semibold pb-5 ">
+          Competidores de liga local
+        </h1>
+        <div className="w-full px-3">
+          <label htmlFor="grid-name">
+            Nombre:{" "}
+            <input
+              type="text"
+              className="appearance-none block w-full mt-2 border border-gray-200
+                rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              name="nombre"
+              value={newCompetidor.nombre}
+              onChange={(e) => handleInputChange(e, setNewCompetidor)}
+            />
+          </label>
+        </div>
+        <div className="w-full px-3 pt-3">
+          <label htmlFor="grid-team">
+            Equipo Insignia:{" "}
+            <select
+				className="appearance-none block w-full mt-2  border border-gray-200
+				rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              name="equipo_insignia"
+              value={newCompetidor.equipo_insignia.id}
+              onChange={(e) =>
+                handleInputChange(e, setNewCompetidor, data.liga.EquipoInsignia)
+              }
+            >
+              {data?.liga?.EquipoInsignia?.map((equipo) => (
+                <option key={equipo.id} value={equipo.id}>
+                  {equipo.nombre}
+                </option>
+              ))}
+            </select>
+            
+          </label>
+        </div>
+        <div className="w-full px-3 pt-3">
+          <label htmlFor="grid-points" />
+          Puntos totales:{" "}
+          <input
+            type="number"
+            name="puntos"
+            min={0}
+            value={newCompetidor.puntos}
+			className="appearance-none block w-full mt-2  border border-gray-200
+			rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            onChange={(e) => handleInputChange(e, setNewCompetidor)}
+          />
+        </div>
+        <button
+          className="mx-3 mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() =>
+            agregarJugador(sendRequest, newCompetidor, setNewCompetidor)
+          }
+        >
+          Guardar
+        </button>
+      </div>
+      <div>
+        <Listado
+          data={data}
+          setData={setData}
+          openModal={openModal}
+          sendRequest={sendRequest}
+        />
+      </div>
+      <AgregarPokemonModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        competidorId={regId}
+        pokemons={data.pokes}
+        ligas={data?.liga?.Liga}
+        rapidos={data.rapidos}
+        cargados={data.cargados}
+        sendRequest={sendRequest}
+      />
+    </div>
+  );
 };
 
 export default LigaLocalAdmin;
