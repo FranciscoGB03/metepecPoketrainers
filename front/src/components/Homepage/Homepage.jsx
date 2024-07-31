cc85eca1795186abeba7175127592de2e8ff30dc
moonclau <ccruzh@toluca.tecnm.mx>
2024-07-30 21:37:30 -0600

    nosotros

--- front/src/components/Homepage/Homepage.jsx
+++ front/src/components/Homepage/Homepage.jsx
@@ -2,6 +2,8 @@ import "./Homepage.css";
 import figureHeader from "../../img/imgchicos.png";
 import { useNavigate } from "react-router-dom";
 import { isTokenExpired } from "../auth/helpers";
+import aboutUsImage from "../../../public/img/aboutus.png";
+
 const Homepage = () => {
   const navigate=useNavigate();  
   const onRegister=()=>{
@@ -31,6 +33,17 @@ const Homepage = () => {
         </div>
       </div>
       <div className="separador-hp"></div>
+      <div className="grid grid-cols-3 gap-4">
+        <div className="about-us col-span-2" >
+          <img src={aboutUsImage}></img>
+        </div>
+        <div className=" w-full p-10">
+          <div className="text-left">
+            <h3 className=" text-4xl font-bold pt-10">Nosotros</h3>
+            <h4 className="text-lg pt-10">Somos un apasionado equipo de programadores y jugadores de Pokémon en Metepec. Nos unimos para crear un espacio donde los mejores entrenadores Pokémon puedan destacar y recibir el reconocimiento que merecen. Nuestra misión es fomentar la comunidad Pokémon, impulsar la excelencia en el entrenamiento y promover la camaradería entre los jugadores.</h4>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
