6202b22f90e52a05c5f5f3c4a26d13043c2d70a7
moonclau <ccruzh@toluca.tecnm.mx>
2024-09-10 21:05:10 -0600

    RelRolPermisos guardado

--- front/src/components/admin/catalogos/RelRolPermisos/RelRolPermisos.jsx
+++ front/src/components/admin/catalogos/RelRolPermisos/RelRolPermisos.jsx
@@ -80,21 +80,23 @@ function RelRolPermisos() {
   };
   /** metodo  para guardar permisos por rol */
   const onSave = async () => {
-    console.log('rol',selectedRole);
-    console.log('permisos',rolePermissions);
-    // await sendRequest(
-    //   "POST",
-    //   SAVE_PERMISSION_BY_ROLE,
-    //   { rol: selectedRole.id, permisos: rolePermissions },
-    //   "save"
-    // );
-    // const response = await sendRequest(
-    //   "GET",
-    //   GET_ALL_PERMISSIONS_BY_ROL + selectedRole.id,
-    //   {},
-    //   "permissionsByRol"
-    // );
-    // setRolePermissions(response || []);
+    if(selectedRole === null){
+     return showErrorAlert("Error: debe seleccionar un rol");
+    }
+    const rol_id=parseInt(selectedRole);
+     await sendRequest(
+       "POST",
+       SAVE_PERMISSION_BY_ROLE,
+       { rol: rol_id, permisos: rolePermissions },
+       "save"
+     );
+     const response = await sendRequest(
+       "GET",
+       GET_ALL_PERMISSIONS_BY_ROL + rol_id,
+       {},
+       "permissionsByRol"
+     );
+     setRolePermissions(response || []);
   };
   /**render */
   return (
@@ -150,136 +152,3 @@ function RelRolPermisos() {
 }
 
 export default RelRolPermisos;
-
-// function RelRolPermisos() {
-//   /**hooks */
-//   const [selectedRole, setSelectedRole] = useState(null);
-//   const [roles, setRoles] = useState([]);
-//   const [permissions, setPermissions] = useState([]);
-//   const [rolePermissions, setRolePermissions] = useState([]);
-//   const { data, error, sendRequest } = useAxiosBack();
-//   /**useEffect */
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       await sendRequest("GET", GET_ALL_PERMISSIONS, {}, "permisos");
-//       await sendRequest("GET", GET_ALL_ROLES, {}, "roles");
-//     };
-//     fetchData();
-//   }, []);
-
-//   useEffect(() => {
-//     if (data.permisos) {
-//       setPermissions(data.permisos);
-//     }
-//     if (data.roles) {
-//       setRoles(data.roles);
-//     }
-//     if (data.permissionsByRol) {
-//       setRolePermissions(data.permissionsByRol || []); // Set to empty array if null
-//     }
-//   }, [data.permisos, data.roles, data.permissionsByRol]);
-
-//   useEffect(() => {
-//     if (data.save) {
-//       showSuccessAlert(data.save.message);
-//     }
-//   }, [data.save]);
-
-//   useEffect(() => {
-//     const message = error?.save?.message;
-//     if (message) {
-//       setTimeout(() => {
-//         showErrorAlert(`Error:${error.save.response.data}, message:${message}`);
-//       }, 200);
-//     }
-//   }, [error]);
-//   /** functions */
-//   const onSelectedRole = async (rol) => {
-//     setSelectedRole(rol);
-//     const response = await sendRequest(
-//       "GET",
-//       GET_ALL_PERMISSIONS_BY_ROL + rol.id,
-//       {},
-//       "permissionsByRol"
-//     );
-//     setRolePermissions(response || []);
-//   };
-
-//   const togglePermission = (permission) => {
-//     setRolePermissions((prevPermissions) => {
-//       const currentPermissions = prevPermissions || [];
-//       if (currentPermissions.some((perm) => perm.id === permission.id)) {
-//         // Quitar permiso
-//         return currentPermissions.filter((perm) => perm.id !== permission.id);
-//       } else {
-//         // Agregar permiso
-//         return [...currentPermissions, permission];
-//       }
-//     });
-//   };
-//   /** metodo  para guardar permisos por rol */
-//   const onSave = async () => {
-//     await sendRequest(
-//       "POST",
-//       SAVE_PERMISSION_BY_ROLE,
-//       { rol: selectedRole.id, permisos: rolePermissions },
-//       "save"
-//     );
-//     const response = await sendRequest(
-//       "GET",
-//       GET_ALL_PERMISSIONS_BY_ROL + selectedRole.id,
-//       {},
-//       "permissionsByRol"
-//     );
-//     setRolePermissions(response || []);
-//   };
-//   /**render */
-//   return (
-//     <div className="role-permission-manager">
-//       <div className="sidebar">
-//         <h2>Roles</h2>
-//         {roles.map((role) => (
-//           <button
-//             key={role.id}
-//             className={`card ${selectedRole?.id === role.id ? "active" : ""}`}
-//             onClick={() => onSelectedRole(role)}
-//           >
-//             <h3>{role.nombre}</h3>
-//           </button>
-//         ))}
-//       </div>
-
-//       <div className="content">
-//         {selectedRole ? (
-//           <div className="card">
-//             <h2>Permisos para {selectedRole.nombre}</h2>
-//             <div>
-//               <button onClick={() => onSave()}>Guardar</button>
-//             </div>
-//             {permissions.map((permission) => (
-//               <div key={permission.id}>
-//                 <label>
-//                   <input
-//                     type="checkbox"
-//                     checked={rolePermissions.some(
-//                       (perm) => perm.id === permission.id
-//                     )}
-//                     onChange={() => togglePermission(permission)}
-//                   />
-//                   {permission.nombre}
-//                 </label>
-//               </div>
-//             ))}
-//             <button onClick={() => setSelectedRole(null)}>Volver</button>
-//           </div>
-//         ) : (
-//           <div className="card">
-//             <h2>Selecciona un rol para ver y editar los permisos</h2>
-//           </div>
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default RelRolPermisos;
